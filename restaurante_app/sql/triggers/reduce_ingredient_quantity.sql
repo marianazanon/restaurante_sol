448de550958dd4027534e3d69ff2270aa1abdfd7
mariana zanon <100360917+marianazanon@users.noreply.github.com>
2024-11-04 17:10:31 -0300

    pequeno ajuste nos triggers

--- restaurante_app/sql/triggers/reduce_ingredient_quantity.sql
+++ restaurante_app/sql/triggers/reduce_ingredient_quantity.sql
@@ -3,9 +3,9 @@ AFTER INSERT ON restaurante_app_venda
 FOR EACH ROW
 BEGIN
     DECLARE qtd_needed INT;
-    
+
     UPDATE restaurante_app_ingredientes AS ing
     JOIN restaurante_app_usos AS uso ON uso.ingrediente_id = ing.id
-    SET ing.quantidade = ing.quantidade - 1
+    SET ing.quantidade = ing.quantidade - NEW.quantidade
     WHERE uso.prato_id = NEW.prato_id;
 END;
