174daa86a4dc2ea6afc0a77ca89a2cd7a0d0c6ee
Gavury <99884688+Gavury@users.noreply.github.com>
2024-11-04 15:42:33 -0300

    Add files via upload

---
+++ restaurante_app/sql/triggers/reduce_ingredient_quantity.sql
@@ -0,0 +1,11 @@
+CREATE TRIGGER reduce_ingredient_quantity_after_sale
+AFTER INSERT ON restaurante_app_venda
+FOR EACH ROW
+BEGIN
+    DECLARE qtd_needed INT;
+    
+    UPDATE restaurante_app_ingredientes AS ing
+    JOIN restaurante_app_usos AS uso ON uso.ingrediente_id = ing.id
+    SET ing.quantidade = ing.quantidade - 1
+    WHERE uso.prato_id = NEW.prato_id;
+END;
